9c5fc941fc4e0a7812898056fc53b84f476b015f
Hasnu <107215022+Hasnu555@users.noreply.github.com>
2024-11-18 21:41:55 +0500

    project started

---
+++ src/component/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div>
+      <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
+        <img src="https://images.unsplash.com/photo-1690401767645-595de0e0e5f8?q=80&w=1426&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Lets Start</p>
+          <div className="container w-100">
+            <select className="m-2 h-100 w-20 bg-success rounded">
+              {Array.from(Array(6), (e, i) => {
+                return (
+                  <option key={i + 1} value={i + 1}>
+                    {i + 1}
+                  </option>
+                );
+              })}
+            </select>
+            <select className="m-2 h-100 w-20 bg-success rounded">
+              <option value="half">Half</option>
+              <option value="full">Full</option>
+            </select>
+            <div className="d-inline h-100 fs-5">Total Price</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/component/Carousal.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+export default function Carousal() {
+  return (
+    <div
+      id="carouselExampleControls"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-inner" id="carousel">
+        <div
+          className="carousel-caption d-none d-md-block"
+          style={{ zIndex: "10" }}
+        >
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-dark text-white bg-dark"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+
+        <div className="carousel-item active">
+          <img
+            src="https://plus.unsplash.com/premium_photo-1673108852141-e8c3c22a4a22?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="d-block w-100"
+            alt="..."
+            style={{ filter: "brightness(60%)" }}
+          />
+        </div>
+
+        <div className="carousel-item">
+          <img
+            src="https://plus.unsplash.com/premium_photo-1670601440146-3b33dfcd7e17?q=80&w=1538&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="d-block w-100"
+            alt="..."
+            style={{ filter: "brightness(60%)" }}
+          />
+        </div>
+
+        <div className="carousel-item">
+          <img
+            src="https://images.unsplash.com/photo-1460306855393-0410f61241c7?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="d-block w-100"
+            alt="..."
+            style={{ filter: "brightness(60%)" }}
+          />
+        </div>
+      </div>
+
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+}
